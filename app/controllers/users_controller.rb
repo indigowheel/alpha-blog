94ff6d3e3c8ff06dc9f5ee4c8ca2eb04a8864f8a
Rob Sterba <robsterba@gmail.com>
2016-12-13 12:12:28 +0000

    complete authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   
-    def index
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
+  
+  def index
     @users = User.paginate(page: params[:page], per_page: 5) 
   end
   
@@ -19,22 +22,19 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
-      
     else
       render 'edit'
     end
   end
   
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
@@ -43,5 +43,15 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
   
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def require_same_user
+    if !logged_in? || current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
+  end
   
 end
